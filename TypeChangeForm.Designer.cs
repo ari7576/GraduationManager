019cdc97f45c653524937737d003f2b67f12f7c5
kimshoon <aristata7576@gmail.com>
2026-06-01 00:59:04 +0900

    커밋

---
+++ TypeChangeForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class TypeChangeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboType = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboType
+            // 
+            this.cboType.FormattingEnabled = true;
+            this.cboType.Location = new System.Drawing.Point(13, 60);
+            this.cboType.Name = "cboType";
+            this.cboType.Size = new System.Drawing.Size(121, 20);
+            this.cboType.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(140, 60);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(89, 35);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "확인";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click_1);
+            // 
+            // TypeChangeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 177);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.cboType);
+            this.Name = "TypeChangeForm";
+            this.Text = "TypeChangeForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboType;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ TypeChangeForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class TypeChangeForm : Form
+    {
+        public string SelectedType { get; private set; }
+        public TypeChangeForm(string currentType)
+        {
+            InitializeComponent();
+
+            cboType.Items.AddRange(new string[] { "전공필수", "전공선택", "전공탐색", "대학교양", "교양기초" });
+            cboType.SelectedItem = currentType;
+        }
+        
+
+        private void btnOk_Click_1(object sender, EventArgs e)
+        {
+            SelectedType = cboType.SelectedItem.ToString();
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+    
+}
